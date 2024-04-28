0ed6373de4f015ef55657436fca1bda76a3e33d9
Cocomelon <Cocomelon.josiapranatalaia@gmail.com>
2024-04-28 16:09:19 +0700

    Pertemuan 03

---
+++ application/views/v_about.php
@@ -0,0 +1,24 @@
+<section> 
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4> 
+    <ul type="disc"> 
+        <li>Nama Depan : Josia</li> 
+        <li>Nama Belakang : Pranata</li> 
+    </ul> 
+    <br> 
+    <h4>Alamat</h4> 
+    <ul type="none"> 
+        <li> Jalan Pekayon Raya No. 45 Bekasi</li> 
+    </ul> 
+    
+    <h4>Tempat Lahir</h4> 
+    <ul type="none"> 
+        <li>Cirebon</li> 
+    </ul> 
+    
+    <h4>Olah Raga Favorit</h4> 
+    <ul type="square"> 
+        <li>Futsal</li> 
+        <li>Renang</li> 
+    </ul> 
+</section>
